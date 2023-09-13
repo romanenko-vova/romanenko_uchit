e6029efcaf3f9b317a86859cc670819b72409d2e
romanenko_vova <bonyshd@gmail.com>
2023-09-13 18:17:43 +0300

    добавил главную страницу

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def main(request):
-    return HttpResponse('Главная')
+    return render(request, 'mainapp/index.html')
